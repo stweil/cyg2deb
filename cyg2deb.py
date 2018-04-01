33eab8728cc2d7ff850e91e8d974c254b347b7c3
Stefan Weil <sw@weilnetz.de>
2018-04-01 22:47:05 +0200

    New package generator and serveral other small fixes and improvements
    - Replace "alien" by "dpkg -b" as it supports dependencies
      and other details.
    
    - Calculate size needed for installed package.
    
    - Fix links for include files.
    
    - Check code with pycodestyle and clean it.
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- cyg2deb.py
+++ cyg2deb.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
 
+# cyg2deb - convert Cygwin packages to Debian packages
+
+import getpass
 import glob
 import http.client
 import os
 import re
 import shutil
+import socket
 import sys
 import tarfile
 
-#~ PROXYSERVER = 'ftp.cygwin.com'
-#~ PROXYPATH = '/pub/cygwin/'
+# TODO: allow selection of proxy server.
+if False:
+    PROXYSERVER = 'ftp.cygwin.com'
+    PROXYPATH = '/pub/cygwin/'
+else:
+    PROXYSERVER = 'ftp-stud.hs-esslingen.de'
+    PROXYPATH = '/pub/Mirrors/sources.redhat.com/cygwin/'
 
-PROXYSERVER = 'ftp-stud.hs-esslingen.de'
-PROXYPATH = '/pub/Mirrors/sources.redhat.com/cygwin/'
+# TODO: get better default e-mail address.
+if os.getenv('EMAIL'):
+    EMAIL = os.getenv('EMAIL')
+else:
+    EMAIL = getpass.getuser() + '@' + socket.getfqdn()
 
 HOSTARCH = 'x86_64'
 
@@ -28,6 +40,7 @@ else:
 
 CACHEDIR = 'cache' + '/' + HOSTARCH
 
+
 # Cygwin package handling.
 class Package:
     def __init__(self, name):
@@ -35,7 +48,7 @@ class Package:
         # Sets name and resets all other values.
         self.name = name
         self.sdesc = ''
-        self.ldesc = ''
+        self.ldesc = []
         self.category = ''
         self.requires = ''
         self.version = ''
@@ -57,18 +70,18 @@ class Package:
         # TODO: check sha512sum.
         name = self.name.lower().replace('_', '-')
         version = self.version.replace('_', '-')
-        filename = name + '_' + version + '-1_' + self.arch + '.deb'
+        packagename = name + '_' + version + '_' + self.arch + '.deb'
         if package.arch != 'all':
-            print("skip package,", filename, '(wrong architecture)')
+            print("skip package,", packagename, '(wrong architecture)')
         elif package.category == '_obsolete':
-            print("skip package,", filename, '(obsolete)')
-        elif os.path.exists(filename):
-            print("skip package,", filename, '(cached)')
+            print("skip package,", packagename, '(obsolete)')
+        elif os.path.exists(packagename):
+            print("skip package,", packagename, '(cached)')
         else:
             # Package file missing in cache, so get it.
             package.printinfo()
             print("")
-            print("make package,", filename)
+            print("make package,", packagename)
             print("")
             filename = CACHEDIR + '/' + self.install
             if not os.path.exists(filename):
@@ -76,7 +89,7 @@ class Package:
                 c = http.client.HTTPSConnection(PROXYSERVER)
                 c.request('GET', PROXYPATH + self.install)
                 r = c.getresponse()
-                #~ print(r.status, r.reason)
+                # print(r.status, r.reason)
                 f = open(filename, 'wb')
                 f.write(r.read())
                 f.close
@@ -84,61 +97,91 @@ class Package:
             tmpdir = CACHEDIR + '/tmp'
             if os.path.exists(tmpdir):
                 shutil.rmtree(tmpdir)
-            #~ try:
-            if 1:
-                f = tarfile.open(filename)
-                f.extractall(tmpdir)
-                f.close()
-                archdir = '/usr/' + ARCH + '-w64-mingw32'
-                srcdir = tmpdir + archdir + '/sys-root/mingw/lib/pkgconfig'
-                if os.path.exists(srcdir):
-                    # Package uses pkg-config.
-                    # Make the *.pc file(s) available at the right location.
-                    dstdir = tmpdir + archdir + '/lib/pkgconfig'
-                    os.makedirs(dstdir)
-                    for path, dirs, files in os.walk(srcdir):
-                        for filename in files:
-                            srcfile = os.path.join('../../sys-root/mingw/lib/pkgconfig', filename)
-                            dstfile = os.path.join(dstdir, filename)
-                            os.symlink(srcfile, dstfile)
-                else:
-                    # Package does not use pkg-config.
-                    # Make the *.h and *.a file(s) available at the right location.
-                    dstdir = tmpdir + archdir + '/include'
-                    os.makedirs(dstdir)
-                    srcdir = tmpdir + archdir + '/sys-root/mingw/include'
-                    for path, dirs, files in os.walk(srcdir):
-                        for filename in files:
-                            srcfile = os.path.join('../sys-root/mingw/include', filename)
-                            dstfile = os.path.join(dstdir, filename)
-                            os.symlink(srcfile, dstfile)
-                    dstdir = tmpdir + archdir + '/lib'
-                    os.makedirs(dstdir)
-                    srcdir = tmpdir + archdir + '/sys-root/mingw/lib'
-                    for path, dirs, files in os.walk(srcdir):
-                        for filename in files:
-                            srcfile = os.path.join('../sys-root/mingw/lib', filename)
-                            dstfile = os.path.join(dstdir, filename)
-                            os.symlink(srcfile, dstfile)
-                filename = CACHEDIR + '/' + self.name + '.tar'
-                f = tarfile.open(filename, 'w')
-                for dir in glob.glob(tmpdir + '/*'):
-                    f.add(dir, arcname = os.path.basename(dir))
-                f.close()
-                #~ print('/usr/bin/fakeroot', '/usr/bin/alien', '--to-deb', '--keep-version', '--description="' + self.sdesc + '"', '--version=' + version, '--target=all', filename)
-                os.spawnlp(os.P_WAIT, '/usr/bin/fakeroot', 'fakeroot', '/usr/bin/alien', '--to-deb', '--keep-version', '--description=' + self.sdesc, '--version=' + version, '--target=all', filename)
-                os.remove(filename)
-            #~ except:
-                #~ print('failed')
+
+            f = tarfile.open(filename)
+            f.extractall(tmpdir)
+            f.close()
+            archdir = '/usr/' + ARCH + '-w64-mingw32'
+            srcdir = tmpdir + archdir + '/sys-root/mingw/lib/pkgconfig'
+            if os.path.exists(srcdir):
+                # Package uses pkg-config.
+                # Make the *.pc file(s) available at the right location.
+                dstdir = tmpdir + archdir + '/lib/pkgconfig'
+                os.makedirs(dstdir)
+                for path, dirs, files in os.walk(srcdir):
+                    for filename in files:
+                        srcfile = '../../sys-root/mingw/lib/pkgconfig/' + \
+                                  filename
+                        dstfile = os.path.join(dstdir, filename)
+                        os.symlink(srcfile, dstfile)
+            else:
+                # Package does not use pkg-config.
+                # Make *.h and *.a file(s) available at the right location.
+                dstdir = tmpdir + archdir + '/include'
+                os.makedirs(dstdir)
+                srcdir = tmpdir + archdir + '/sys-root/mingw/include'
+                for srcfile in glob.glob(os.path.join(srcdir, '*')):
+                    filename = srcfile.replace(srcdir + '/', '')
+                    srcfile = '../sys-root/mingw/include/' + filename
+                    dstfile = os.path.join(dstdir, filename)
+                    os.symlink(srcfile, dstfile)
+                dstdir = tmpdir + archdir + '/lib'
+                os.makedirs(dstdir)
+                srcdir = tmpdir + archdir + '/sys-root/mingw/lib'
+                for path, dirs, files in os.walk(srcdir):
+                    for filename in files:
+                        srcfile = '../sys-root/mingw/lib/' + filename
+                        dstfile = os.path.join(dstdir, filename)
+                        os.symlink(srcfile, dstfile)
+
+            kbytes = 0
+            for path, dirs, files in os.walk(tmpdir):
+                for filename in files:
+                    filename = os.path.join(path, filename)
+                    blocks = os.lstat(filename).st_blocks
+                    kbytes += int((blocks + 3) / 2)
+            kbytes = str(kbytes)
+            # print(kbytes, 'KiB')
+
+            dstdir = tmpdir + '/DEBIAN'
+            os.makedirs(dstdir)
+            lf = "\n"
+            f = open(dstdir + '/control', 'w', encoding='utf-8')
+            f.write('Package: ' + self.name + lf)
+            f.write('Version: ' + self.version + lf)
+            f.write('Architecture: all\n')
+            if EMAIL:
+                f.write('Maintainer: ' + EMAIL + lf)
+            f.write('Installed-Size: ' + kbytes + lf)
+            f.write('Origin: Cygwin\n')
+            depends = self.requires
+            depends = depends.replace('mingw64-i686-gcc-core',
+                                      'mingw-w64-i686-dev')
+            depends = depends.replace('mingw64-i686-gcc-g++', '')
+            depends = depends.replace('mingw64-i686-pkg-config',
+                                      'pkg-config-mingw-w64-i686')
+            depends = depends.replace('  ', ',')
+            depends = depends.replace(' ', ',')
+            f.write('Depends: ' + depends + lf)
+            f.write('Section: cygwin\n')
+            f.write('Priority: extra\n')
+            f.write('Homepage: https://github.com/stweil/cyg2deb/\n')
+            f.write('Description: ' + self.sdesc + lf)
+            for line in self.ldesc:
+                f.write(' ' + line + lf)
+            f.close()
+
+            # print('dpkg', '-b', tmpdir, packagename)
+            os.spawnlp(os.P_WAIT, 'dpkg', 'dpkg', '-b', tmpdir, packagename)
 
     def printinfo(self):
         # Print some information for package.
-        #~ self.ldesc = ''
-        #~ self.install_sha512sum = ''
-        #~ self.source = ''
-        #~ self.source_size = 0
-        #~ self.source_sha512sum = ''
-        #~ self.message = ''
+        # self.ldesc = []
+        # self.install_sha512sum = ''
+        # self.source = ''
+        # self.source_size = 0
+        # self.source_sha512sum = ''
+        # self.message = ''
         print("Name:        " + self.name)
         print("Version:     " + self.version)
         print("Category:    " + self.category)
@@ -146,8 +189,6 @@ class Package:
         print("Description: " + self.sdesc)
         print("Install:     " + self.install + " (" + self.install_size + ")")
 
-
-#~ alien --to-deb --generate --keep-version --description "Zlib for Win64 toolchain" --version 1.2.8-4 --target=all
 
 os.makedirs(CACHEDIR, 0o777, True)
 
@@ -159,14 +200,12 @@ if not os.path.exists(setup_ini):
     conn.request('GET', PROXYPATH + HOSTARCH + '/setup.ini')
     response = conn.getresponse()
     # 200 OK
-    #~ print(response.status, response.reason)
+    # print(response.status, response.reason)
     data = response.read()
     conn.close()
     f = open(setup_ini, "wb")
     f.write(data)
     f.close()
-
-f = open(setup_ini)
 
 do_ldesc = False
 do_message = False
@@ -174,19 +213,20 @@ do_skip = False
 
 package = False
 
+f = open(setup_ini)
+
 for line in f:
     line = line.strip()
-    #~ print(line)
-    #~ continue
+    # print(line)
+    # continue
 
     if do_ldesc:
-        package.ldesc += ' '
         m = re.search('(.*)"', line)
         if m:
-            package.ldesc += m.group(1)
+            package.ldesc.append(m.group(1))
             do_ldesc = False
         else:
-            package.ldesc += line
+            package.ldesc.append(line)
         continue
 
     if do_message:
@@ -231,10 +271,10 @@ for line in f:
 
     m = re.search('^ldesc: "(.*)', line)
     if m:
-        package.ldesc = m.group(1)
+        package.ldesc = [m.group(1)]
         m = re.search('^ldesc: "(.*)"', line)
         if m:
-            package.ldesc = m.group(1)
+            package.ldesc = [m.group(1)]
         else:
             do_ldesc = True
         continue
@@ -282,8 +322,5 @@ for line in f:
         else:
             do_message = True
         continue
-
-    #~ print(package, " ???")
-    #~ print(line)
 
 f.close()
